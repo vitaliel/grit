6ab736807a681976073e3677cbe960c26c1b3b01
Vladimir Zdorovenco <breakunit@gmail.com>
2009-07-16 13:18:20 +0300

    Fix git.shell_escape

--- lib/grit/git.rb
+++ lib/grit/git.rb
@@ -1,5 +1,7 @@
+require 'shellwords'
+
 module Grit
-  
+
   class Git
     class GitTimeout < RuntimeError
       attr_reader :command, :bytes_read
@@ -11,36 +13,37 @@ module Grit
     end
 
     undef_method :clone
-    
+
     include GitRuby
-    
+
     class << self
       attr_accessor :git_binary, :git_timeout, :git_max_size
     end
-  
+
     self.git_binary   = "/usr/bin/env git"
     self.git_timeout  = 10
     self.git_max_size = 5242880 # 5.megabytes
-    
+
     def self.with_timeout(timeout = 10.seconds)
       old_timeout = Grit::Git.git_timeout
       Grit::Git.git_timeout = timeout
       yield
       Grit::Git.git_timeout = old_timeout
     end
-    
+
     attr_accessor :git_dir, :bytes_read
-    
+
     def initialize(git_dir)
       self.git_dir    = git_dir
       self.bytes_read = 0
     end
-    
+
     def shell_escape(str)
-      str.to_s.gsub("'", "\\\\'").gsub(";", '\\;')
+      # str.to_s.gsub("'", "\\\\'").gsub(";", '\\;')
+      Shellwords.shellescape(str.to_s)
     end
     alias_method :e, :shell_escape
-    
+
     # Run the given git command with the specified arguments and return
     # the result as a String
     #   +cmd+ is the command
@@ -60,7 +63,7 @@ module Grit
       timeout  = true if timeout.nil?
 
       opt_args = transform_options(options)
-      ext_args = args.reject { |a| a.empty? }.map { |a| (a == '--' || a[0].chr == '|') ? a : "'#{e(a)}'" }
+      ext_args = args.reject { |a| a.empty? }.map { |a| (a == '--' || a[0].chr == '|') ? a : "#{e(a)}" }
 
       call = "#{prefix}#{Git.git_binary} --git-dir='#{self.git_dir}' #{cmd.to_s.gsub(/_/, '-')} #{(opt_args + ext_args).join(' ')}#{e(postfix)}"
       Grit.log(call) if Grit.debug
@@ -136,5 +139,5 @@ module Grit
       args
     end
   end # Git
-  
+
 end # Grit
